5a41b69f4da39c02a16950a9433d0dfaced13531
mmartin46 <mmartin46@angelo.edu>
2024-06-08 06:14:19 -0500

    Changes made

--- site.client/src/App.jsx
+++ site.client/src/App.jsx
@@ -3,31 +3,22 @@ import { useEffect, useState, useRef, useReducer } from 'react';
 
 // For all files
 import Header from './components/headers/Header.jsx';
-/*import SecondaryHeader from './components/SecondaryHeader.jsx';*/
 import Footer from './components/footers/Footer.jsx';
-import axios from 'axios';
-import { Outlet, Link } from "react-router-dom";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import './index.css';
-import PageCarousel from './components/general/PageCarousel.jsx';
 import SecondaryHeader from './components/headers/SecondaryHeader.jsx';
 import CartLayout from './components/layouts/authentification/CartLayout.jsx';
 import RegisterLayout from './components/layouts/authentification/RegisterLayout.jsx';
 import LoginLayout from './components/layouts/authentification/LoginLayout.jsx';
-import HeaderProducts from './components/sections/HeaderProducts.jsx';
-
-import headImage from 'C:\\Users\\mitch\\Downloads\\WebApp\\Site\\site.client\\src\\images\\headimage.jpg';
-import shutterImage from 'C:\\Users\\mitch\\Downloads\\shutterstock_266498825.jpg';
-import BodySection from './components/sections/BodySection.jsx';
 import { ShoesLayout, PantsLayout, ShirtsLayout } from './components/layouts/general/MainLayouts.jsx';
 import ProductLayout from './components/layouts/general/ProductLayout.jsx';
 import { MainLayout } from './components/layouts/general/MainLayout.jsx';
 
-const ScreenRoutes = (props) => {
+const ScreenRoutes = () => {
     return (
        <Routes>
            <Route path="/" element={<MainLayout />} />
