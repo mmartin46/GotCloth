670a960a80aa46f0afe83e5bd722a797f835c744
mmartin46 <mmartin46@angelo.edu>
2024-06-21 08:27:52 -0500

    total bug fixed

--- site.client/src/components/layouts/authentification/CartLayout.jsx
+++ site.client/src/components/layouts/authentification/CartLayout.jsx
@@ -95,6 +95,7 @@ const CartLayout = () => {
 
     const handleRemoveProduct = (name) => {
         setCartItems(prevItems => prevItems.filter(item => item.title.includes(name)));
+        appendTotal();
     };
 
     return (
